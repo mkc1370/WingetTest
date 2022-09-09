b3d50b63dc446eb9834c41408ed6a8161ffc00d6
mkc1370 <mkc1370go@gmail.com>
2022-09-10 01:03:12 +0900

    wingetでの自動インストールのサンプルを追加

---
+++ auto_install.ps1
@@ -0,0 +1,15 @@
+# ref : https://www.cats-insteadof-pc.net/wpdb/index.php/2021/12/31/runas/
+# 管理者権限で実行されていなければ、スクリプトを管理者権限で実行し直す
+If (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] 'Administrator')){
+    Start-Process -FilePath PowerShell.exe -ArgumentList "-NoLogo -ExecutionPolicy Bypass -File $($MyInvocation.MyCommand.Path)" -Verb RunAs
+    Exit
+}
+
+# カレントディレクトリをps1のある位置に変更する
+Set-Location -Path $PSScriptRoot
+
+# パッケージの一覧を読み込んで、並列でインストールする
+$packages = Get-Content packages.txt
+foreach($package in $packages){
+    Start-Process powershell "winget install -e --id $package"
+}
